a1de07103053fb238ae03e75bbc93b1aa883f2b3
Eduardo Flavio de Carvalho <edufcarvalho@gmail.com>
2021-10-16 21:19:51 -0300

    Contato da página index refatorado com efeitos novos

--- js/modules/hideElement.js
+++ js/modules/hideElement.js
@@ -1,15 +1,11 @@
 export default function initHideElement() {
 
-  const botaoElemento = document.getElementById("hide-element");
+  const botaoContato = document.getElementById("hide-element");
 
-  if (botaoElemento !== null) {
-    function hideEl() {
-      botaoElemento.nextElementSibling.classList.toggle("ocultar");
-    }
-  
-    botaoElemento.addEventListener('click', hideEl);
-    hideEl();
+  function hideContato() {
+    this.nextElementSibling.classList.toggle('ativo');
   }
-  
+
+  botaoContato.addEventListener('click', hideContato);
 
 }
